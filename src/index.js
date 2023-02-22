f51db57a78b4503632f9dbadeada7cf331fdca47
Courtney Hampton <jaye0888@gmail.com>
2023-02-22 15:02:52 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import listEndpoints from "express-list-endpoints";
 import mongoose from "mongoose";
 import server from "./server.js";
 
-const port = process.env.PORT;
+const port = process.env.PORT | 3001;
 
 mongoose.connect(process.env.MONGO_URL);
 
